55c9dd2b7343bdddb4896827df26b91c57611855
Abdulaziz <abdulazizawlw@gmail.com>
2023-12-30 07:52:59 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,99 @@
+// Create web server
+const express = require('express');
+const app = express();
+
+// Create a route
+app.get('/comment', (req, res) => {
+    res.send('Hello World');
+});
+
+// Start the server
+app.listen(3000, () => console.log('Listening on port 3000'));
+
+// Run the file
+// node comment.js
+
+// Go to the browser
+// http://localhost:3000/comment
+// Hello World
+
+// Go to the terminal and press Ctrl+C to stop the server
+
+// Path: comment.js
+// Create web server
+const express = require('express');
+const app = express();
+
+// Create a route
+app.get('/comment', (req, res) => {
+    res.send('Hello World');
+});
+
+// Start the server
+app.listen(3000, () => console.log('Listening on port 3000'));
+
+// Run the file
+// node comment.js
+
+// Go to the browser
+// http://localhost:3000/comment
+// Hello World
+
+// Go to the terminal and press Ctrl+C to stop the server
+
+// Path: comment.js
+// Create web server
+const express = require('express');
+const app = express();
+
+// Create a route
+app.get('/comment', (req, res) => {
+    res.send('Hello World');
+});
+
+// Start the server
+app.listen(3000, () => console.log('Listening on port 3000'));
+
+// Run the file
+// node comment.js
+
+// Go to the browser
+// http://localhost:3000/comment
+// Hello World
+
+// Go to the terminal and press Ctrl+C to stop the server
+
+// Path: comment.js
+// Create web server
+const express = require('express');
+const app = express();
+
+// Create a route
+app.get('/comment', (req, res) => {
+    res.send('Hello World');
+});
+
+// Start the server
+app.listen(3000, () => console.log('Listening on port 3000'));
+
+// Run the file
+// node comment.js
+
+// Go to the browser
+// http://localhost:3000/comment
+// Hello World
+
+// Go to the terminal and press Ctrl+C to stop the server
+
+// Path: comment.js
+// Create web server
+const express = require('express');
+const app = express();
+
+// Create a route
+app.get('/comment', (req, res) => {
+    res.send('Hello World');
+});
+
+// Start the server
+app
